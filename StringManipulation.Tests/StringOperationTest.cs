a87fcbf5f4b92f97d5b1c9a2226a1bbd68a4ae8d
Giancarlo Culcay <gieg5c@gmail.com>
2023-08-07 13:40:51 -0500

    Primera Prueba

---
+++ StringManipulation.Tests/StringOperationTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;  //para que funcione Fact
+
+namespace StringManipulation.Tests
+{
+    public class StringOperationTest //siempre las clases de prueba deben ser públicas
+    {
+        [Fact]
+        public void ConcatenateStrings()
+        {
+            var strOperation = new StringOperations();
+            var result = strOperation.ConcatenateStrings("Hello", "World");
+
+            Assert.Equal("Hello World", result); //esperado, resultado
+        }
+    }
+}
